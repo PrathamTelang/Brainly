11dbae75ccffbd4d2c863bc509495fbd325bd328
PrathamTelang <prathamtelang007@gmail.com>
2025-07-09 14:07:41 +0530

    Initial commit
    Project scaffolding and initial file setup.

---
+++ backend/src/db.ts
@@ -0,0 +1,15 @@
+import mongoose, { model, Schema } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL as string;
+
+mongoose.connect(MONGO_URL);
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true },
+  password: String
+});
+
+export const UserModel = model("User", UserSchema);
---
+++ backend/src/index.ts
@@ -0,0 +1,76 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { UserModel } from "./db";
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const PORT = process.env.PORT;
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "get route"
+    })
+})
+
+app.post("/api/v1/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    try {
+        await UserModel.create({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message: "User signed up"
+    })
+    } catch(e) {
+        res.status(411).json({
+            message: "user already exists"
+        })
+    }
+})
+
+app.post("api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser = await UserModel.findOne({
+        username: username,
+        password: password,
+    })
+})
+
+app.post("api/v1/content", (req, res) => {
+    
+})
+
+app.get("api/v1/content", (req, res) => {
+    
+})
+
+
+app.delete("api/v1/content", (req, res) => {
+    
+})
+
+
+app.post("api/v1/brain/share", (req, res) => {
+    
+})
+
+app.post("api/v1/brain/:shareLink", (req, res) => {
+    
+})
+
+
+export default app;
+
+app.listen(parseInt(PORT || "5000"), () => {
+  console.log(`🚀 Server running on http://localhost:${PORT || "5000"}`);
+});
