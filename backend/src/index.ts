617442b31b6759c6487ad4e782cb075d57c63eb1
PrathamTelang <prathamtelang007@gmail.com>
2025-07-09 19:25:06 +0530

    Add JWT authentication to signin endpoint
    Installed @types/jsonwebtoken for type support and updated the /api/v1/signin route to generate and return a JWT token upon successful authentication. Also fixed the signin route path and improved type safety for JWT_SECRET.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken"
 import { UserModel } from "./db";
 dotenv.config();
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+
 const PORT = process.env.PORT;
 
 const app = express()
@@ -37,13 +40,27 @@ app.post("/api/v1/signup", async (req, res) => {
     }
 })
 
-app.post("api/v1/signin", async (req, res) => {
+app.post("/api/v1/signin", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+
     const existingUser = await UserModel.findOne({
         username: username,
         password: password,
     })
+    if (existingUser) {
+    const token = jwt.sign(
+      { id: existingUser._id },
+      JWT_SECRET
+    );
+    res.json({
+        token
+    }); 
+} else {
+    res.status(403).json({
+        message: "Invalid credentials"
+    })
+} 
 })
 
 app.post("api/v1/content", (req, res) => {
